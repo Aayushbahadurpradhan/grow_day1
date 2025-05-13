e9fb9cd44049ca04f35473501e24bf2ff258cede
Aayushbahadurpradhan <pradhanaayush9841@gmail.com>
2025-05-13 11:33:57 +0545

    week 4 day2

--- week4/day4/src/features/api/jsonPlaceholderApi.js
+++ week4/day4/src/features/api/jsonPlaceholderApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const jsonPlaceholderApi = createApi({
   reducerPath: 'jsonPlaceholderApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com/' }),
-  tagTypes: ['Users', 'Posts', 'Todos', 'ErrorLogs'],
+  tagTypes: ['Users'],
   refetchOnReconnect: true,
   refetchOnFocus: true,
   endpoints: (builder) => ({
@@ -28,54 +28,6 @@ export const jsonPlaceholderApi = createApi({
       invalidatesTags: ['Users'],
     }),
 
-    // Posts
-    getPosts: builder.query({
-      query: () => 'posts',
-      providesTags: ['Posts'],
-    }),
-    addPost: builder.mutation({
-      query: (post) => ({
-        url: 'posts',
-        method: 'POST',
-        body: post,
-      }),
-      invalidatesTags: ['Posts'],
-    }),
-    deletePost: builder.mutation({
-      query: (id) => ({
-        url: `posts/${id}`,
-        method: 'DELETE',
-      }),
-      invalidatesTags: ['Posts'],
-    }),
-
-    // Todos
-    getTodos: builder.query({
-      query: () => 'todos',
-      providesTags: ['Todos'],
-    }),
-    addTodo: builder.mutation({
-      query: (todo) => ({
-        url: 'todos',
-        method: 'POST',
-        body: todo,
-      }),
-      invalidatesTags: ['Todos'],
-    }),
-    deleteTodo: builder.mutation({
-      query: (id) => ({
-        url: `todos/${id}`,
-        method: 'DELETE',
-      }),
-      invalidatesTags: ['Todos'],
-    }),
-
-    // Error Logs (Polling Demo)
-    getErrorLogs: builder.query({
-      query: () => 'comments',
-      providesTags: ['ErrorLogs'],
-      pollingInterval: 5000,
-    }),
   }),
 })
 
@@ -83,11 +35,5 @@ export const {
   useGetUsersQuery,
   useAddUserMutation,
   useDeleteUserMutation,
-  useGetPostsQuery,
-  useAddPostMutation,
-  useDeletePostMutation,
-  useGetTodosQuery,
-  useAddTodoMutation,
-  useDeleteTodoMutation,
-  useGetErrorLogsQuery,
+  
 } = jsonPlaceholderApi
