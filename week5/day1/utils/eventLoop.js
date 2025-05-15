95a79e5b260bedbe75c95fec25586b5a4b1ac43b
Aayushbahadurpradhan <pradhanaayush9841@gmail.com>
2025-05-15 18:07:29 +0545

    eventLoop file

---
+++ week5/day1/utils/eventLoop.js
@@ -0,0 +1,13 @@
+
+const path = require('path');
+const { spawn } = require('child_process');
+function runEventLoop() {
+  const scriptPath = path.join(__dirname, '../logger.js');
+  const child = spawn('node', [scriptPath], {
+    stdio: 'inherit',
+  });
+  child.on('exit', (code) => {
+    console.log(`Event loop demo exited with code ${code}`);
+  });
+}
+module.exports = runEventLoop;
