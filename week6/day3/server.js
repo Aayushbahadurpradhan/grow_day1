bf52cb470e0677aa645884a0dac1d65d9d1f2638
Aayushbahadurpradhan <pradhanaayush9841@gmail.com>
2025-05-26 15:26:48 +0545

    week6 day3

---
+++ week6/day3/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const connectDB = require('./config/db');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+connectDB();
+app.use('/api/employees', require('./routes/employeeRoutes'));
+app.use('/api/departments', require('./routes/departmentRoutes'));
+
+const errorHandler = require('./utils/errorHandler');
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
