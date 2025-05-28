c773ada16612b8dc1207c702b1ac4a444791a83f
Aayushbahadurpradhan <pradhanaayush9841@gmail.com>
2025-05-28 17:09:12 +0545

    adding new routes of payroll

--- week6/day5/routes/payrollRoutes.js
+++ week6/day5/routes/payrollRoutes.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const payrollController  = require('../controllers/payrollController');
+const {
+  createPayroll,
+  getPayrollsByEmployee,
+  getEmployeesSortedByNetPay,
+  getPayrollById,
+  deletePayroll
+} = require('../controllers/payrollController');
 
-router.post('/', payrollController.createPayroll);
-router.get('/:id', payrollController.getPayrollsByEmployee);
+router.post('/', createPayroll);
+router.get('/employee/:id', getPayrollsByEmployee); 
+router.get('/sorted/netpay', getEmployeesSortedByNetPay); 
+router.get('/:id', getPayrollById); 
+router.delete('/:id', deletePayroll); 
 
 module.exports = router;
