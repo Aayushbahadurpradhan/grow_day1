d6e9a8fd54df7aab8eb1c4bc4880966f1a8aaa2b
Aayushbahadurpradhan <pradhanaayush9841@gmail.com>
2025-05-28 17:06:47 +0545

    adding delete and sorted by net pay

--- week6/day5/controllers/payrollController.js
+++ week6/day5/controllers/payrollController.js
@@ -3,7 +3,6 @@ const { handlePayrollTransaction } = require('../services/payrollService');
 
 exports.createPayroll = async (req, res) => {
   const { employeeId, baseSalary, bonus, deductions } = req.body;
-
   try {
     const payroll = await handlePayrollTransaction(employeeId, baseSalary, bonus, deductions);
     res.status(201).json({ message: 'Payroll created successfully', payroll });
@@ -16,6 +15,56 @@ exports.getPayrollsByEmployee = async (req, res) => {
   try {
     const payrolls = await Payroll.find({ employeeId: req.params.id });
     res.status(200).json(payrolls);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.getEmployeesSortedByNetPay = async (req, res) => {
+  try {
+    const { page = 1, limit = 10 } = req.query;
+    const skip = (page - 1) * parseInt(limit);
+    const result = await Payroll.aggregate([
+      {
+        $lookup: {
+          from: 'employees',
+          localField: 'employeeId',
+          foreignField: '_id',
+          as: 'employee'
+        }
+      },
+      { $unwind: '$employee' },
+      { $sort: { netPay: -1 } },
+      { $skip: skip },
+      { $limit: parseInt(limit) },
+      {
+        $project: {
+          _id: 0,
+          employeeId: 1,
+          netPay: 1,
+          name: {
+            $concat: ['$employee.first_name', ' ', '$employee.last_name']
+          },
+          departmentId: '$employee.departmentId',
+          baseSalary: '$employee.baseSalary'
+        }
+      }
+    ]);
+
+    res.json(result);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+
+exports.deletePayroll = async (req, res) => {
+  try {
+    const deleted = await Payroll.findByIdAndDelete(req.params.id);
+    if (!deleted) {
+      return res.status(404).json({ message: 'Payroll not found' });
+    }
+    res.status(200).json({ message: 'Payroll deleted successfully' });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
