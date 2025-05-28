f663e621c9324ba38a61c098b1337d7a5336f58e
Aayushbahadurpradhan <pradhanaayush9841@gmail.com>
2025-05-28 17:09:30 +0545

    week6 day 5

--- week6/day5/server.js
+++ week6/day5/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-require('dotenv').config();
+const errorHandler = require('./utils/errorHandler');
+
+dotenv.config(); 
 
 const app = express();
+
 app.use(express.json());
 
 connectDB();
@@ -11,7 +15,6 @@ app.use('/api/employees', require('./routes/employeeRoutes'));
 app.use('/api/departments', require('./routes/departmentRoutes'));
 app.use('/api/payroll', require('./routes/payrollRoutes'));
 
-const errorHandler = require('./utils/errorHandler');
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
