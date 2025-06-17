5611581f71790ee33ee85d3356b7a672e23db828
Aayushbahadurpradhan <pradhanaayush9841@gmail.com>
2025-06-17 21:56:56 +0545

    frontend folder

---
+++ week9/day2/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import WeatherCard from './components/WeatherCard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Weather + CORS </h1>
+      <WeatherCard />
+    </div>
+  );
+}
+
+export default App;
